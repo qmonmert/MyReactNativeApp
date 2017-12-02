fa030aa10e0b4d5cde1d66a8c33c22117eb2d936
Quentin Monmert <qmonmert@mbp-de-cedric.home>
2017-12-02 20:03:35 +0100

    Add a component

---
+++ Activities.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Activities extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Activities</Text>
+      </View>
+    );
+  }
+}
